b6c746a13d8dbb3ec4c5af7c914ec9f4e98cd794
Omkar-codes0105 <omkarjoshi.13245@gmail.com>
2026-02-20 13:33:30 +0530

    Update eyetracking __init__.py to export KalmanTracker and CalibrationManager

--- src/eyetracking/__init__.py
+++ src/eyetracking/__init__.py
@@ -1,4 +1,6 @@
 """Eye tracking module."""
 from .eye_tracker import EyeTracker
+from .kalman_tracker import KalmanTracker
+from .calibration import CalibrationManager
 
-__all__ = ['EyeTracker']
+__all__ = ['EyeTracker', 'KalmanTracker', 'CalibrationManager']
